2123f1da5245da6d2b83e0bca011bb9bcf5b9cb0
Jose Henrique <vasco0007@gmail.com>
2018-07-23 02:23:13 -0300

    transformado app em componente e criado inicio da busca de videos

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 // create a new component. This component should produce some HTML
 
 //React: create and manage our components
 //ReactDOM: render files
 
-//api key: AIzaSyCbjKBxzrOZyXC5ARs6ZPpCMEliOzStAqU
-
 const API_KEY = 'AIzaSyCbjKBxzrOZyXC5ARs6ZPpCMEliOzStAqU'; //google API Key
 
 
 //every variable like const is the final value to this variable, dont change anymore
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'manhuacu'}, videos => {
+            this.setState({ videos });
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
