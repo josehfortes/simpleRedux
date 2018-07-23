667b0d4bd14e38565583d6371d0f9e4231b84550
Jose Henrique <vasco0007@gmail.com>
2018-07-23 13:33:20 -0300

    busca de video basica funcionando

--- src/index.js
+++ src/index.js
@@ -23,7 +23,11 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({key: API_KEY, term: 'manhuacu'}, videos => {
+        this.videoSearch('aviao');
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term}, videos => {
             this.setState({ 
                 videos:videos,
                 selectedVideo: videos[0]
@@ -34,7 +38,7 @@ class App extends Component {
     render(){
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
                 <VideoDetail  video={this.state.selectedVideo}/>
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
