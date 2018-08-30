8db236bd2ab69cec1080ffbc3339a73f38c4095b
JOSE HENRIQUE FORTES LEITE <jose.fortes@embraer.com.br>
2018-08-30 13:27:11 -0300

    configurado reducer_weather para tratar casos do tipo FETCH WEATHER

--- 3-metereologia/src/reducers/reducer_weather.js
+++ 3-metereologia/src/reducers/reducer_weather.js
@@ -1,5 +1,11 @@
-export default function(state = null, action){
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action){
     //a resposta da promisse aparece aqui
-    console.log("ACTION RECEIVED", action);
+    switch (action.type){
+        case FETCH_WEATHER:
+            return [action.payload.data, ...state];
+
+    }
     return state;
 }
