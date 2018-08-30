56743f6495c5e84a052060003e03194d1148732c
JOSE HENRIQUE FORTES LEITE <jose.fortes@embraer.com.br>
2018-08-30 15:28:36 -0300

    inserido cidade via google maps

--- 3-metereologia/src/containers/weather_list.js
+++ 3-metereologia/src/containers/weather_list.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import LineChart from '../components/line_chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
     renderWeather(cityData){
         const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+        const { lon, lat } = cityData.city.coord;
+
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lat={lat} lon={lon} /></td>
+                <td>
+                    <LineChart data={temps} color="blue"/>
+                </td>
+                <td>
+                    <LineChart data={pressures} color="red"/>
+                </td>
+                <td>
+                    <LineChart data={humidities} color="green"/>
+                </td>
             </tr>
         )
     }
@@ -17,9 +33,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hP)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
